37ae942743e4b46bb239b049dcd7ff119b728be8
Enrique Teruel <144461422+eteruel-paymefy@users.noreply.github.com>
2023-10-22 01:04:30 +0200

    feat(react): react import refactor

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,8 @@
-// React imports 
-import { registerReactControllerComponents } from '@symfony/ux-react';
-registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
-// start the Stimulus application
+// start the Stimulus and React application
 import './bootstrap';
 
 
--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,8 +1,13 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge';
+import { registerReactControllerComponents } from '@symfony/ux-react';
+
+// Registers React components as Stimulus controllers
+registerReactControllerComponents(require.context('./react/components', true, /\.(j|t)sx?$/));
+
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context(
-    '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
+    '@symfony/stimulus-bridge/lazy-controller-loader!./react/components',
     true,
     /\.[jt]sx?$/
 ));
